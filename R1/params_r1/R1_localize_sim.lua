4df5e4c9952baadffff81ea0bb53970e7d4cc40d
Tin LetHax <thipok17@gmail.com>
2026-06-20 19:47:22 +0700

    Lowered the pose graph localization score to make the re-localization faster.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- R1/params_r1/R1_localize_sim.lua
+++ R1/params_r1/R1_localize_sim.lua
@@ -6,14 +6,14 @@ MAP_BUILDER.num_background_threads = 6
 TRAJECTORY_BUILDER.pure_localization_trimmer = {max_submaps_to_keep = 10,}
 
 POSE_GRAPH.optimize_every_n_nodes = 1 -- Original 1
-POSE_GRAPH.constraint_builder.min_score = 0.80  -- Modify 0.55 to 0.65, the minium score of Fast csm, can be optimized above this score 
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.80  -- Modify 0.6 as 0.7, Minimum global positioning score below which global positioning is considered currently inaccurate
-POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 100.0
+POSE_GRAPH.constraint_builder.min_score = 0.55  -- Modify 0.55 to 0.65, the minium score of Fast csm, can be optimized above this score 
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.65  -- Modify 0.6 as 0.7, Minimum global positioning score below which global positioning is considered currently inaccurate
+POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1.0
 -- POSE_GRAPH.constraint_builder.ceres_scan_matcher.rotation_weight = 1.0
 -- POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 100
 
 -- For localization mode
--- POSE_GRAPH.constraint_builder.max_constraint_distance = 1.0
+POSE_GRAPH.constraint_builder.max_constraint_distance = 1.0
 
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(90)
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 0.5
